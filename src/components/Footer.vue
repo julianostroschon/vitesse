58ebe46b002849e4917f12ea7d44152df1ccbe8f
Juliano Stroschon <julianostroschon@gmail.com>
2022-11-10 20:59:10 -0300

    ⚡️ Visando ficar com nota máxima, alterado uma tag `a` para `button``

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -18,9 +18,9 @@ const toggleLocales = () => {
       <div i="carbon-sun dark:carbon-moon" />
     </button>
 
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
+    <button class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
       <div i-carbon-language />
-    </a>
+    </button>
 
     <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
       <div i-carbon-dicom-overlay />
